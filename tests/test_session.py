97938e443505be69c2c9697c98ebfa134eadde74
Javier Valero Cejudo <javalce29@gmail.com>
2024-03-05 13:18:05 +0100

    Refactor test_get_session function in test_session.py

--- tests/test_session.py
+++ tests/test_session.py
@@ -2,16 +2,15 @@ from sqlalchemy.orm import Session
 from sqlautils.session import _session, get_session
 
 
-def test_get_session():
-    # Test when session is available
+def test_get_session_when_available():
     session = Session()
     _session.set(session)
     assert get_session() == session
 
-    # Test when session is not available
+
+def test_get_session_when_not_available():
     _session.set(None)
     try:
         get_session()
-        raise AssertionError("Expected RuntimeError")
     except RuntimeError as e:
         assert str(e) == "No session is available"
